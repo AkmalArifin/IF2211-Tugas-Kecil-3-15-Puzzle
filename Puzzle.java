0beda4d661cc3090be485237b4bf27dd8e01ea0e
AkmalArifin <13520037@std.stei.itb.ac.id>
2022-04-04 21:02:45 +0700

    Merge

---
+++ Puzzle.java
@@ -0,0 +1,19 @@
+public class Puzzle
+{
+    public int[][] puzzle;
+    final int[][] finalPuzzle = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,0}};
+    public PriorityQueue<E> pqueue = new PriorityQueue<E>();
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSolveable() {
+        return true;
+    }
+
+    public int[][] solve(int[][] puzzle) {
+        return puzzle;  
+    }
+
+}
